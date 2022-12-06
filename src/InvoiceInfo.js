5c5825a3b80b71cb227ff9ab54686e8987841d71
noarea147 <95benmbarek@gmail.com>
2022-12-06 02:19:55 +0100

    FIXESS

--- src/InvoiceInfo.js
+++ src/InvoiceInfo.js
@@ -3,16 +3,14 @@ import "./App.css";
 import React, { useState } from "react";
 import Papa from "papaparse";
 import axios from "axios";
+import { Dna } from "react-loader-spinner";
 
 function InvoiceInfo() {
-  const [host, setHost] = useState();
-  const [user, setUser] = useState();
-  const [pass, setPass] = useState();
   const [subject, setSubject] = useState();
   const [sendername, setSendername] = useState();
   const [emailbody, setEmailbody] = useState();
   const allowedExtensions = ["csv"];
-  const [data, setData] = useState([]);
+  const [isSending, setIsSending] = useState(false);
   const [error, setError] = useState("");
   const [file, setFile] = useState("");
 
@@ -32,146 +30,115 @@ function InvoiceInfo() {
   const handleParse = async (e) => {
     e.preventDefault();
     if (!file) return setError("Enter a valid file");
+    setIsSending(true);
     const reader = new FileReader();
     reader.onload = async ({ target }) => {
       const csv = Papa.parse(target.result, { header: true });
       const parsedData = csv?.data;
-      let payload = {
-        emailData: {
-          senderName: sendername,
-          subject: subject,
-          emailBody: emailbody
-        },
-        smtp: {
-          host: host,
-          user: user,
-          pass: pass
-        },
-        emails: parsedData
-      };
-      let response = await axios({
-        url: "https://sendmailsapp.herokuapp.com/send",
-        header: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-        },
-        method: "POST",
-        data: payload
-      });
-      console.log(response);
+      for (let i = 0; i < parsedData.length; i++) {
+        let payload = {
+          emailData: {
+            senderName: sendername,
+            subject: subject,
+            emailBody: emailbody
+          },
+          email: parsedData[i].email
+        };
+        let response = await axios({
+          url: "http://localhost:4000/send",
+          // header: {
+          //   "Access-Control-Allow-Origin": "*",
+          //   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+          // },
+          method: "POST",
+          data: payload
+        });
+        if (response.status === 200) {
+          alert(i + 1 + " email sent successfully");
+          console.log(i)
+        }
+        if (i+1  == parsedData.length || response.status !== 200) {
+          setIsSending(false);
+        }
+      }
     };
     reader.readAsText(file);
   };
 
   return (
-    <div id="wrapper" className="container">
-      <div style={{}}>
-        <br />
-        <center>
-          <img src={logo} height="200px" />
-        </center>
-        <br />
-      </div>
+    <>
+      {isSending ? (
+        <div
+          style={{
+            display: "flex",
+            height: "600px",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column"
+          }}
+        >
+          <Dna
+            visible={true}
+            height="120"
+            width="120"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+          <br />
+          <p>Hold On it's in my DNA</p>
+        </div>
+      ) : (
+        <div id="wrapper" className="container">
+          <div style={{}}>
+            <br />
+            <center>
+              <img src={logo} height="200px" />
+            </center>
+            <br />
+          </div>
 
-      <div className="row">
-        <div className="col-12">
-          <div id="html_div">
-            <h3>email configurations</h3>
-            <form className="form-group" onSubmit={handleParse}>
-              <div>
-                <input
-                  type="text"
-                  placeholder="email host"
-                  className="form-control"
-                  onChange={(e) => setHost(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="form-control"
-                  onChange={(e) => setUser(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="email password"
-                  className="form-control"
-                  onChange={(e) => setPass(e.target.value)}
-                />
-              </div>
-
-              <h3> email info </h3>
+          <div className="row">
+            <div className="col-12">
               <div id="html_div">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="sender name (name will be displayed) ex: Barry greyman"
-                  onChange={(e) => setSendername(e.target.value)}
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="subject"
-                  onChange={(e) => setSubject(e.target.value)}
-                />
-                <br />
-                <h5>Tip:</h5>
-                <p>
-                  <p style={{ color: "red" }}>
-                    {" "}
-                    use this word in your body to set it as a dynamic word
-                  </p>
-                  <strong>[[name]]</strong>
-                  <br />
-                  <p style={{ color: "red" }}>
-                    exemple: if i wanna send email body like this :
-                  </p>
-                  hey Alaa
-                  <br />
-                  i hope you are okey <br />
-                  best regards . <br />
-                  <p style={{ color: "red" }}>
-                    i should use this tool like this
-                  </p>
-                  hey [[name]] <br />
-                  i hope you are okey <br />
-                  best regards . <br />
-                </p>
+                <h3>Priv8 Advenced Mailing Tool</h3>
+                <form className="form-group" onSubmit={handleParse}>
+                  <div id="html_div">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="sender name (name will be displayed) ex: Barry greyman"
+                      onChange={(e) => setSendername(e.target.value)}
+                    />
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="subject"
+                      onChange={(e) => setSubject(e.target.value)}
+                    />
+                    <br />
 
-                <textarea
-                  name="w3review"
-                  rows="4"
-                  cols="90"
-                  className="form-control"
-                  onChange={(e) => setEmailbody(e.target.value)}
-                >
-                  email content should be here don't for get to use [[name]].
-                  feel free to delete this
-                </textarea>
-                <p>
-                  upload your excel file in CSV extension <br /> how ?{" "}
-                  <a target="_blank" href="https://prnt.sc/Cd8NGSdolUJ9">
-                    {" "}
-                    click here
-                  </a>
-                </p>
-                <p>
-                  how my file should look <br /> you don't know ?{" "}
-                  <a target="_blank" href="https://prnt.sc/oMyP-Mivvv-m">
-                    {" "}
-                    click here
-                  </a>
-                </p>
-                <center>
-                  <input type="file" onChange={handleFileChange} />
-                </center>
+                    <textarea
+                      name="w3review"
+                      rows="4"
+                      cols="90"
+                      className="form-control"
+                      onChange={(e) => setEmailbody(e.target.value)}
+                      placeholder="letter html"
+                    ></textarea>
+                    <center>
+                      <input type="file" onChange={handleFileChange} />
+                    </center>
 
-                <input type="submit" value="send" />
+                    <input type="submit" value="send" />
+                  </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
